c6d86ce24b4d939b9a6eb437294f030a63e9a5b2
Guilherme G. <ep.0.season.@gmail.com>
2024-06-30 15:15:34 -0300

    Att.

--- Projeto/DesafioControleFluxo/src/Contador.java
+++ Projeto/DesafioControleFluxo/src/Contador.java
@@ -5,26 +5,28 @@ import java.util.Scanner;
 
 public class Contador {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner terminal = new Scanner(System.in);
         
         System.out.println(("Digite o primeiro parâmetro: "));
-            int parametroUm = scanner.nextInt(); 
+            int parametroUm = terminal.nextInt(); 
         
             System.out.println(("Digite o segundo parâmetro: "));
-            int parametroDois = scanner.nextInt();
+            int parametroDois = terminal.nextInt();
 
         try {         
             //chamando o método contendo a lógica de contagem
             contar(parametroUm, parametroDois);
 
         } catch(InputMismatchException e){
-            System.err.println("Erro inesperado: Insira um número inteiro." + e.getMessage());
+            System.out.println("Erro inesperado: Insira um número inteiro." + e.getMessage());
         } catch (IllegalArgumentException e) {
-            System.out.println("Erro inesperado: ");
+            System.out.println("O primeiro valor precisa ser maior do que o segundo");
             //Imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro.
-            //System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+            //System.out.println("O segundo parâmetro deve ser maior  que o primeiro");
+        }catch(Exception e){
+            System.out.println("Erro inesperado" + e.getMessage());
         }
-        scanner.close();
+        terminal.close();
     }
     static void contar(int parametroUm, int parametroDois) {
         //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
